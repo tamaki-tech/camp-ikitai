d787655d29ae55c96654ac6de4e45dab7c13f78d
kusotamaki <oshaburimegane@gmail.com>
2021-08-15 12:40:19 +0900

    リファクタ

---
+++ camp-ikitai-front/components/facility/FacilityExistPart.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div v-for="facility in facilities" :key="facility.id">
+      <v-row>
+        <v-col cols="4">{{ facility.facilityName }}</v-col>
+        <v-col cols="8">{{ facility.isExist }}</v-col>
+      </v-row>
+      <v-divider></v-divider>
+    </div>
+    <br />
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'nuxt-property-decorator'
+import Facility from '@/domains/campSite/Facility'
+
+@Component
+export default class FacilityExistPart extends Vue {
+  @Prop()
+  facilities!: Facility[]
+}
+</script>
