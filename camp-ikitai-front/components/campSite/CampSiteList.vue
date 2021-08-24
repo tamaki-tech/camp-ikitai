8edaf8ad6551f13b5f3a4c999815c15b0d82f90f
kusotamaki <oshaburimegane@gmail.com>
2021-08-24 18:37:00 +0900

    検索結果画面の項目追加

--- camp-ikitai-front/components/campSite/CampSiteList.vue
+++ camp-ikitai-front/components/campSite/CampSiteList.vue
@@ -13,7 +13,42 @@
           <v-img contain max-height="100" max-width="100" :src="site.imagePath" />
         </v-col>
         <v-col cols="8">
-          <p>{{ `${site.address.pref} ${site.address.city} ${site.address.otherAddress}` }}</p>
+          <v-row no-gutters>
+            <div class="text-caption">
+              <strong>
+                料金: {{ site.price }} 円
+              </strong>
+            </div>
+          </v-row>
+          <v-row no-gutters>
+            <div class="text-caption">
+              <strong>
+                アクセス: {{ site.access }}
+              </strong>
+            </div>
+          </v-row>
+         <v-row no-gutters>
+           <v-col cols="6">
+            <div class="text-caption">
+              <strong>
+                イキタイ
+              </strong>
+              <v-chip color="red" outlined x-small>
+               {{ site.ikitai }}
+              </v-chip>
+            </div>
+           </v-col>
+           <v-col cols="6">
+             <div class="text-caption">
+               <strong>
+                クチコミ
+               </strong>
+               <v-chip color="red" outlined x-small>
+                {{ site.reviewCount }}
+               </v-chip>
+             </div>
+           </v-col>
+         </v-row>
         </v-col>
       </v-row>
     </v-sheet>
