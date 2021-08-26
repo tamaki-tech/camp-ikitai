41ca52e4a4665b5e2d22dff21f78bc635aac24f7
kusotamaki <oshaburimegane@gmail.com>
2021-08-26 18:53:10 +0900

    stubJsonの参照パス変更

--- camp-ikitai-front/domains/campSite/CampSiteServiceStub.ts
+++ camp-ikitai-front/domains/campSite/CampSiteServiceStub.ts
@@ -3,17 +3,13 @@ import CampSiteService from '@/domains/campSite/CampSiteService'
 import CampSiteInfo from '@/domains/campSite/CampSiteInfo'
 type Dictionary<T> = { [key: string]: T }
 
-// TODO 暫定
-const environment = process.env.NODE_ENV || 'local'
-const env = require(`./config/${environment}.ts`)
-
 export default class CampSiteServiceStub implements CampSiteService {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     async search(query: Dictionary<string | (string | null)[]>): Promise<CampSiteInfo[]> {
-        return ((await axios.get(`${env.BASE_URL}/data/content/CampSiteSearch.json`)).data) as CampSiteInfo[]
+        return ((await axios.get('../data/content/CampSiteSearch.json')).data) as CampSiteInfo[]
     }
 
     async searchById(id: string): Promise<CampSiteInfo> {
-        return ((await axios.get(`${env.BASE_URL}/data/content/CampSiteInfo/${id}.json`)).data) as CampSiteInfo
+        return ((await axios.get(`../data/content/CampSiteInfo/${id}.json`)).data) as CampSiteInfo
     }
 }
