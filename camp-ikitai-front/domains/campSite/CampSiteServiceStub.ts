90b8cf2204c188b7b5f57b8fef1fe490ccf37019
kusotamaki <oshaburimegane@gmail.com>
2021-08-26 19:01:35 +0900

    baseurlをstubのjson参照パスに追加

--- camp-ikitai-front/domains/campSite/CampSiteServiceStub.ts
+++ camp-ikitai-front/domains/campSite/CampSiteServiceStub.ts
@@ -6,10 +6,10 @@ type Dictionary<T> = { [key: string]: T }
 export default class CampSiteServiceStub implements CampSiteService {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     async search(query: Dictionary<string | (string | null)[]>): Promise<CampSiteInfo[]> {
-        return ((await axios.get('../data/content/CampSiteSearch.json')).data) as CampSiteInfo[]
+        return ((await axios.get(`${process.env.BASE_URL}data/content/CampSiteSearch.json`)).data) as CampSiteInfo[]
     }
 
     async searchById(id: string): Promise<CampSiteInfo> {
-        return ((await axios.get(`../data/content/CampSiteInfo/${id}.json`)).data) as CampSiteInfo
+        return ((await axios.get(`${process.env.BASE_URL}data/content/CampSiteInfo/${id}.json`)).data) as CampSiteInfo
     }
 }
