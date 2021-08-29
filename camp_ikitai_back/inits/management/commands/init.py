0f0e13f8d685b4b0dc47387b579458d8f1d02bcb
kusotamaki <oshaburimegane@gmail.com>
2021-08-30 08:04:57 +0900

    campSiteアプリケーション追加

--- camp_ikitai_back/inits/management/commands/init.py
+++ camp_ikitai_back/inits/management/commands/init.py
@@ -1,8 +1,17 @@
 # -*- coding:utf-8 -*-
 
+import subprocess
 from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        self.stdout.write("Hello World!")
+        # start docker
+        # TODO すでにコンテナ起動してたら落として再作成する？
+        subprocess.call("docker-compose up -d", shell=True)
+
+        # run migrate
+        subprocess.call("python manage.py makemigrations", shell=True)
+        subprocess.call("python manage.py migrate", shell=True)
+
+        # TODO 初期データ投入
