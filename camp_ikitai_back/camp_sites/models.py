ba48525e32ba30425dc4d74efd9f63541185e4ba
tamaki-tech <oshaburimegane@gmail.com>
2021-09-04 11:05:50 +0900

    テーブルレイアウト変更

--- camp_ikitai_back/camp_sites/models.py
+++ camp_ikitai_back/camp_sites/models.py
@@ -1,12 +1,19 @@
 from django.db import models
 
+
 class SiteType(models.Model):
     name = models.CharField(max_length=30)
+
+    class Meta:
+        db_table = 'site_type'
 
 
 class User(models.Model):
     name = models.CharField(max_length=30)
     mail_address = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = 'user'
 
 
 class CampSite(models.Model):
@@ -19,20 +26,28 @@ class CampSite(models.Model):
     price = models.IntegerField()
     image_path = models.ImageField(upload_to="images/")
 
+    class Meta:
+        db_table = 'camp_site'
+
 
 class Address(models.Model):
-    camp_site_id = models.ForeignKey(CampSite, unique=True, on_delete=models.PROTECT)
+    camp_site_id = models.OneToOneField(CampSite, on_delete=models.PROTECT)
     zip_code = models.CharField(blank=True, null=True, max_length=10)
     pref_name = models.CharField(blank=True, null=True, max_length=30)
     city_name = models.CharField(blank=True, null=True, max_length=30)
     other_address = models.CharField(blank=True, null=True, max_length=30)
 
+    class Meta:
+        db_table = 'address'
+
 
 class Coordinate(models.Model):
-    camp_site_id = models.ForeignKey(
-        CampSite, unique=True, on_delete=models.PROTECT)
+    camp_site_id = models.OneToOneField(CampSite, on_delete=models.PROTECT)
     let = models.FloatField()
     lon = models.FloatField()
+
+    class Meta:
+        db_table = 'coordinate'
 
 
 class Facility(models.Model):
@@ -40,10 +55,16 @@ class Facility(models.Model):
     name = models.CharField(blank=True, null=True, max_length=30)
     is_exist = models.BooleanField(default=False)
 
+    class Meta:
+        db_table = 'facility'
+
 
 class Ikitai(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.PROTECT)
     camp_site_id = models.ForeignKey(CampSite, on_delete=models.PROTECT)
+
+    class Meta:
+        db_table = 'ikitai'
 
 
 class Review(models.Model):
@@ -51,3 +72,6 @@ class Review(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.PROTECT)
     point = models.IntegerField(default=0)
     Comment = models.TextField()
+
+    class Meta:
+        db_table = 'review'
