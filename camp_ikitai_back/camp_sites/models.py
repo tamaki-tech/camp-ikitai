ca0783df149c0701b26c1e7a49081553e265cf7e
tamaki-tech <oshaburimegane@gmail.com>
2021-09-04 11:22:23 +0900

    model再修正

--- camp_ikitai_back/camp_sites/models.py
+++ camp_ikitai_back/camp_sites/models.py
@@ -1,3 +1,4 @@
+from enum import unique
 from django.db import models
 
 
@@ -5,7 +6,7 @@ class SiteType(models.Model):
     name = models.CharField(max_length=30)
 
     class Meta:
-        db_table = 'site_type'
+        db_table = "site_type"
 
 
 class User(models.Model):
@@ -13,11 +14,11 @@ class User(models.Model):
     mail_address = models.CharField(max_length=100)
 
     class Meta:
-        db_table = 'user'
+        db_table = "user"
 
 
 class CampSite(models.Model):
-    site_type_id = models.ForeignKey(SiteType, on_delete=models.PROTECT)
+    site_type = models.ForeignKey(SiteType, on_delete=models.PROTECT)
     name = models.CharField(max_length=30)
     addition = models.TextField(null=True, blank=True, max_length=300)
     access = models.CharField(max_length=30)
@@ -27,51 +28,57 @@ class CampSite(models.Model):
     image_path = models.ImageField(upload_to="images/")
 
     class Meta:
-        db_table = 'camp_site'
+        db_table = "camp_site"
 
 
 class Address(models.Model):
-    camp_site_id = models.OneToOneField(CampSite, on_delete=models.PROTECT)
+    camp_site = models.OneToOneField(
+        CampSite, on_delete=models.PROTECT, primary_key=True)
     zip_code = models.CharField(blank=True, null=True, max_length=10)
     pref_name = models.CharField(blank=True, null=True, max_length=30)
     city_name = models.CharField(blank=True, null=True, max_length=30)
     other_address = models.CharField(blank=True, null=True, max_length=30)
 
     class Meta:
-        db_table = 'address'
+        db_table = "address"
 
 
 class Coordinate(models.Model):
-    camp_site_id = models.OneToOneField(CampSite, on_delete=models.PROTECT)
+    camp_site = models.OneToOneField(
+        CampSite, on_delete=models.PROTECT, primary_key=True)
     let = models.FloatField()
     lon = models.FloatField()
 
     class Meta:
-        db_table = 'coordinate'
+        db_table = "coordinate"
 
 
 class Facility(models.Model):
-    camp_site_id = models.ForeignKey(CampSite, on_delete=models.PROTECT)
+    camp_site = models.ForeignKey(CampSite, on_delete=models.PROTECT)
     name = models.CharField(blank=True, null=True, max_length=30)
     is_exist = models.BooleanField(default=False)
 
     class Meta:
-        db_table = 'facility'
+        db_table = "facility"
 
 
 class Ikitai(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.PROTECT)
-    camp_site_id = models.ForeignKey(CampSite, on_delete=models.PROTECT)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    camp_site = models.ForeignKey(CampSite, on_delete=models.PROTECT)
 
     class Meta:
-        db_table = 'ikitai'
+        db_table = "ikitai"
+        constraints = [
+            models.UniqueConstraint(
+                fields=["user", "camp_site"], name="unique_ikitai")
+        ]
 
 
 class Review(models.Model):
-    camp_site_id = models.ForeignKey(CampSite, on_delete=models.PROTECT)
-    user_id = models.ForeignKey(User, on_delete=models.PROTECT)
+    camp_site = models.ForeignKey(CampSite, on_delete=models.PROTECT)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
     point = models.IntegerField(default=0)
     Comment = models.TextField()
 
     class Meta:
-        db_table = 'review'
+        db_table = "review"
