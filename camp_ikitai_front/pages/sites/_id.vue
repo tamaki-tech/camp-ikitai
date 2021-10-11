dfa18afc294d48de97d1361f6ba1490287969166
tamaki-tech <oshaburimegane@gmail.com>
2021-10-12 07:54:23 +0900

    SNSアイコン部分をリファクタ

--- camp_ikitai_front/pages/sites/_id.vue
+++ camp_ikitai_front/pages/sites/_id.vue
@@ -22,26 +22,7 @@
               </h3>
             </v-col>
           </v-row>
-          <v-row v-if="!$vuetify.breakpoint.mobile">
-            <v-col cols="2">
-              <v-btn color="secondary" outlined>
-                <v-img src="icon/twitter.svg" height="20px" width="20px" />
-                <strong>ツイートする</strong>
-              </v-btn>
-            </v-col>
-            <v-col cols="2">
-              <v-btn color="secondary" outlined>
-                <v-img src="icon/line.svg" height="20px" width="20px" />
-                <strong>友達に教える</strong>
-              </v-btn>
-            </v-col>
-            <v-col cols="2">
-              <v-btn color="secondary" outlined>
-                <v-img src="icon/facebook.svg" height="25px" width="25px" />
-                <strong>シェアする</strong>
-              </v-btn>
-            </v-col>
-          </v-row>
+          <sns-icons />
           <v-row>
             <v-col>
               <h4>■設備・ルール</h4>
