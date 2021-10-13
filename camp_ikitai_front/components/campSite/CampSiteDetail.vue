7b246b176e43a709b282e5702252ac03c6d423d9
tamaki-tech <oshaburimegane@gmail.com>
2021-10-13 22:00:07 +0900

    詳細画面のアイコン追加に伴う修正

--- camp_ikitai_front/components/campSite/CampSiteDetail.vue
+++ camp_ikitai_front/components/campSite/CampSiteDetail.vue
@@ -1,47 +1,54 @@
 <template>
-  <v-sheet align="center">
-    <v-img contain max-height="500px" :src="campSiteInfo.imagePath" />
-    <br />
+  <div>
     <v-row>
-      <v-col cols="4" align-self="center"><strong>施設名</strong></v-col>
-      <v-col cols="8">{{ campSiteInfo.siteName }}</v-col>
-    </v-row>
-    <v-divider></v-divider>
-    <v-row>
-      <v-col cols="4" align-self="center"><strong>施設タイプ</strong></v-col>
-      <v-col cols="8">{{ isExists(campSiteInfo.type) }}</v-col>
-    </v-row>
-    <v-divider></v-divider>
-    <v-row>
-      <v-col cols="4" align-self="center"><strong>アクセス</strong></v-col>
-      <v-col cols="8">{{ isExists(campSiteInfo.access) }}</v-col>
-    </v-row>
-    <v-divider></v-divider>
-    <v-row>
-      <v-col cols="4" align-self="center"><strong>電話番号</strong></v-col>
-      <v-col cols="8">{{ isExists(campSiteInfo.phoneNumber) }}</v-col>
-    </v-row>
-    <v-divider></v-divider>
-    <v-row>
-      <v-col cols="4" align-self="center"><strong>HP</strong></v-col>
-      <v-col cols="8">
-        <v-btn
-          :href="isExists(campSiteInfo.url)"
-          text
-          color="link"
-          class="x-small post-link align-center py-1 px-2"
-        >
-          <strong>公式サイトを開く</strong>
-        </v-btn>
+      <v-col>
+        <h2>基本情報</h2>
       </v-col>
     </v-row>
-    <v-divider></v-divider>
-    <v-row>
-      <v-col cols="4" align-self="center"><strong>料金</strong></v-col>
-      <v-col cols="8">{{ isExists(campSiteInfo.price) }}</v-col>
-    </v-row>
-    <v-divider></v-divider>
-  </v-sheet>
+    <v-sheet align="center" class="mt-5 mb-5">
+      <v-img contain max-height="500px" :src="campSiteInfo.imagePath" />
+      <br />
+      <v-row>
+        <v-col cols="4" align-self="center"><strong>施設名</strong></v-col>
+        <v-col cols="8">{{ campSiteInfo.siteName }}</v-col>
+      </v-row>
+      <v-divider></v-divider>
+      <v-row>
+        <v-col cols="4" align-self="center"><strong>施設タイプ</strong></v-col>
+        <v-col cols="8">{{ isExists(campSiteInfo.type) }}</v-col>
+      </v-row>
+      <v-divider></v-divider>
+      <v-row>
+        <v-col cols="4" align-self="center"><strong>アクセス</strong></v-col>
+        <v-col cols="8">{{ isExists(campSiteInfo.access) }}</v-col>
+      </v-row>
+      <v-divider></v-divider>
+      <v-row>
+        <v-col cols="4" align-self="center"><strong>電話番号</strong></v-col>
+        <v-col cols="8">{{ isExists(campSiteInfo.phoneNumber) }}</v-col>
+      </v-row>
+      <v-divider></v-divider>
+      <v-row>
+        <v-col cols="4" align-self="center"><strong>HP</strong></v-col>
+        <v-col cols="8">
+          <v-btn
+            :href="isExists(campSiteInfo.url)"
+            text
+            color="link"
+            class="x-small post-link align-center py-1 px-2"
+          >
+            <strong>公式サイトを開く</strong>
+          </v-btn>
+        </v-col>
+      </v-row>
+      <v-divider></v-divider>
+      <v-row>
+        <v-col cols="4" align-self="center"><strong>料金</strong></v-col>
+        <v-col cols="8">{{ isExists(campSiteInfo.price) }}</v-col>
+      </v-row>
+      <v-divider></v-divider>
+    </v-sheet>
+  </div>
 </template>
 
 <script lang="ts">
--- camp_ikitai_front/components/facility/FacilityExistPart.vue
+++ camp_ikitai_front/components/facility/FacilityExistPart.vue
@@ -1,16 +1,35 @@
 <template>
+  <div>
+    <v-row class="mt-5">
+      <v-col>
+        <h2>施設情報</h2>
+      </v-col>
+    </v-row>
     <v-sheet align="center">
       <div v-for="facility in facilities" :key="facility.id">
         <v-row>
-          <v-col cols="4">{{ facility.facilityName }}</v-col>
-          <v-col cols="8">{{ facility.isExist }}</v-col>
+          <v-col cols="2" class="mt-3 mb-4">
+            <v-img
+              contain
+              max-height="42"
+              :src="`icon/facility/${facility.facility}.svg`"
+            />
+          </v-col>
+          <v-col cols="3" class="mt-5 mb-4">
+            <strong>{{ facility.facilityName }}</strong>
+          </v-col>
+          <v-col cols="2" class="mt-2 mb-3">
+            <h1>{{ facility.isExist }}</h1>
+          </v-col>
         </v-row>
         <v-divider></v-divider>
       </div>
       <br />
-    </div>
     </v-sheet>
-
+    <v-btn block elevation="0" color="primary">
+      <strong>全ての施設情報を見る</strong> 
+    </v-btn>
+  </div>
 </template>
 
 <script lang="ts">
