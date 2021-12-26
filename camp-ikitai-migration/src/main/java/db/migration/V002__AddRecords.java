8ae85828ae322166d51993162296e1dc65f24907
tamaki-tech <oshaburimegane@gmail.com>
2021-12-26 22:10:02 +0900

    初期データ投入のお試し

---
+++ camp-ikitai-migration/src/main/java/db/migration/V002__AddRecords.java
@@ -0,0 +1,19 @@
+package db.migration;
+
+import io.sitoolkit.csv.core.CsvLoader;
+import org.flywaydb.core.api.logging.Log;
+import org.flywaydb.core.api.logging.LogFactory;
+import org.flywaydb.core.api.migration.BaseJavaMigration;
+import org.flywaydb.core.api.migration.Context;
+
+@SuppressWarnings("squid:S101")
+public class V002__AddRecords extends BaseJavaMigration {
+
+    private final Log log = LogFactory.getLog(V002__AddRecords.class);
+
+    @Override
+    public void migrate(Context ctx) throws Exception {
+      CsvLoader.load(ctx.getConnection(), getClass(), log::info);
+    }
+
+}
